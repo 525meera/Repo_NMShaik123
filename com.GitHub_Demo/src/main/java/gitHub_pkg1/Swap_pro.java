01790b1f7c45655a4dc1f474ffc32ec192ac1a98
NMShaik <nmshaik525@gmail.com>
2023-07-11 16:23:16 +0530

    commit-swap

---
+++ com.GitHub_Demo/src/main/java/gitHub_pkg1/Swap_pro.java
@@ -0,0 +1,16 @@
+package gitHub_pkg1;
+
+public class Swap_pro 
+{
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c;
+		c=a;
+		a=b;
+		b=c;
+		
+		System.out.println("Swap value "+a+" is "+b);
+
+	}
+}
