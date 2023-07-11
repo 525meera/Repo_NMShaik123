78757acd3227073931a01f524db60c8560b087c4
NMShaik <nmshaik525@gmail.com>
2023-07-11 18:49:53 +0530

    Update Factorials.java

--- com.GitHub_Demo/src/main/java/gitHub_pkg1/Factorials.java
+++ com.GitHub_Demo/src/main/java/gitHub_pkg1/Factorials.java
@@ -4,12 +4,12 @@ public class Factorials
 {
 public static void main(String[] args) {
 
-	int num=26;
+	int num=6;
 
 	int fact=1;
 	for(int i=1;i<=num;i++) {
 		fact=fact*i;
-		System.out.println(fact);
+		//System.out.println(fact);
 	}
 	System.out.println(fact);
 
