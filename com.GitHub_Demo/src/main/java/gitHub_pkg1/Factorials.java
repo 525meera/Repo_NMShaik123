17770f13eafe36e8c43786045f6d0a93b9c20583
NMShaik <nmshaik525@gmail.com>
2023-07-11 18:19:29 +0530

    Create Factorials.java

---
+++ com.GitHub_Demo/src/main/java/gitHub_pkg1/Factorials.java
@@ -0,0 +1,18 @@
+package gitHub_pkg1;
+
+public class Factorials 
+{
+public static void main(String[] args) {
+
+	int num=26;
+
+	int fact=1;
+	for(int i=1;i<=num;i++) {
+		fact=fact*i;
+		System.out.println(fact);
+	}
+	System.out.println(fact);
+	
+	
+}
+}
