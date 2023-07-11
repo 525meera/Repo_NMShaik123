32fc84103e967a1ff7e054f570d2b3ab0f057e40
NMShaik <nmshaik525@gmail.com>
2023-07-11 17:11:26 +0530

    sameAdd

--- com.GitHub_Demo/src/main/java/gitHub_pkg1/Hello_Pro.java
+++ com.GitHub_Demo/src/main/java/gitHub_pkg1/Hello_Pro.java
@@ -4,5 +4,10 @@ public class Hello_Pro
 {
 	public static void main(String[] args) {
 		System.out.println("Hello Program");
+<<<<<<< Updated upstream
+=======
+		System.out.println("Hello India");
+		System.out.println("Hello");
+>>>>>>> Stashed changes
 	}
 }
