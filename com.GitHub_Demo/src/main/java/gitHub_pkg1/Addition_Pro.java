2198805b85ff2c92f48878b39f6003492908f8aa
NMShaik <nmshaik525@gmail.com>
2023-07-11 16:14:18 +0530

    Create_Addition

---
+++ com.GitHub_Demo/src/main/java/gitHub_pkg1/Addition_Pro.java
@@ -0,0 +1,11 @@
+package gitHub_pkg1;
+
+public class Addition_Pro
+{
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("Addition of "+c);
+	}
+}
