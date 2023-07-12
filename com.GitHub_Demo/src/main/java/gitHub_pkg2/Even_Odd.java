ca258d3b48321aea596f4b5fdf7e2b8f4cfa8da7
NMShaik <nmshaik525@gmail.com>
2023-07-12 12:06:17 +0530

    UPDATE

--- com.GitHub_Demo/src/main/java/gitHub_pkg2/Even_Odd.java
+++ com.GitHub_Demo/src/main/java/gitHub_pkg2/Even_Odd.java
@@ -2,14 +2,16 @@ package gitHub_pkg2;
 
 public class Even_Odd 
 {
-public static void main(String[] args) {
-	int a=12;
-	if(a%2==0) {
-		System.out.println("Even Number");
+	public static void main(String[] args) {
+		int a=12;
+		if(a%2==0) {
+			System.out.println("Even Number");
+		}
+		else
+		{
+			System.out.println("Not even numbers");
+		}
+		System.out.println("Successfully Completed");
+
 	}
-	else
-	{
-		System.out.println("Not even numbers");
-	}
-}
 }
