4a77404e7cc0e72c80b283333a7d506d2b62952d
NMShaik <nmshaik525@gmail.com>
2023-07-12 11:58:49 +0530

    SamethingUpdate

--- com.GitHub_Demo/src/main/java/gitHub_pkg2/DataType.java
+++ com.GitHub_Demo/src/main/java/gitHub_pkg2/DataType.java
@@ -5,11 +5,11 @@ public class DataType
 	public static void main(String[] args) {
 		byte a;
 		a= 127;
-		System.out.println("byte value :- "+a);
+		System.out.println("byte value : "+a);
 		short s= -32768;
 		System.out.println("short value"+s);
 		int i= 2147483647;
-		System.out.println("integer value:- "+i);
+		System.out.println("integer value: "+i);
 		long l=9222036854775807l;
 		System.out.println("long value "+l);
 		char c= 'a';
@@ -19,9 +19,9 @@ public class DataType
 		System.out.println("my name is "+ss);
 
 		float f=2.12345678F;
-		System.out.println("float value :- "+f);
+		System.out.println("float value : "+f);
 		double d=123.12345678345678345678234567;
-		System.out.println("Double value :-"+d);
+		System.out.println("Double value : "+d);
 		System.out.println();
 		boolean n=true;
 		boolean m=false;
@@ -29,10 +29,10 @@ public class DataType
 		int r=20;
 
 		if(w>r) {// correct 
-			System.out.println("Boolean:-"+n);
+			System.out.println("Boolean: "+n);
 		}
 		else {// wrong
-			System.out.println("Boolean:-"+m);
+			System.out.println("Boolean: "+m);
 		}
 	}
 }
