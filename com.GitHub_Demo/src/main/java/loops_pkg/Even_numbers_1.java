3e9c8f708367be28b91e0d5f9a58fc9fbe0db498
NMShaik <nmshaik525@gmail.com>
2023-07-14 12:03:15 +0530

    update the code

--- com.GitHub_Demo/src/main/java/loops_pkg/Even_numbers_1.java
+++ com.GitHub_Demo/src/main/java/loops_pkg/Even_numbers_1.java
@@ -2,23 +2,29 @@ package loops_pkg;
 
 public class Even_numbers_1
 {
-public static void main(String[] args) {
-	int n=10;
-	int x = 0;
-	System.out.println("Print first 10 even Numbers: ");
-	while(x<=10) {
+	public static void main(String[] args) {
+		int n = 10;
+		int x = 0;
+
+		System.out.println("Using while loop statement");
+		System.out.println("Print first 10 even Numbers: ");
 		
-		//System.out.println("  "+x);
-		
-		x= x+2;
-		
-	}
-	//System.out.println("Even Number");
-	for(int i=1;i<=n;i++) {
-		if(i%2==0) {
-			System.out.println(i);
+		while(x<=10) 
+		{
+			System.out.println("  "+x);
+			x= x+2;
 		}
+
+		System.out.println("Using for loop statement");
+		System.out.println("Print second 10 Even Number:");
 		
+		for(int i=1;i<=n;i++) 
+		{
+			if(i%2==0) 
+			{
+				System.out.println(i);
+			}
+
+		}
 	}
-}
 }
