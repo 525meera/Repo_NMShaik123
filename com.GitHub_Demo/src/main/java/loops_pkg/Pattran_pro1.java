aba9286c36a952df587e83d26c188a3e2b756d2a
NMShaik <nmshaik525@gmail.com>
2023-07-15 11:21:12 +0530

    NewProAdd

---
+++ com.GitHub_Demo/src/main/java/loops_pkg/Pattran_pro1.java
@@ -0,0 +1,29 @@
+package loops_pkg;
+
+public class Pattran_pro1 
+{
+	public static void main(String[] args) {
+		int row=5;
+		for(int i=row;i>=1;i--) {
+			for(int j=i;j<row;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		int r=5;
+		for(int i=1;i<=r;i++) 
+		{
+			for(int j=r;j>i;j--) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
