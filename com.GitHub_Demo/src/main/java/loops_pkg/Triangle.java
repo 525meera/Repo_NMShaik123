da72585f950119eb7c4da8f7e2531c9e9e74ad46
NMShaik <nmshaik525@gmail.com>
2023-07-15 18:50:36 +0530

    Create Triangle.java

---
+++ com.GitHub_Demo/src/main/java/loops_pkg/Triangle.java
@@ -0,0 +1,29 @@
+package loops_pkg;
+
+public class Triangle 
+{
+	public static void main(String[] args) {
+		int row=5;
+		for(int i=1;i<=row;i++) 
+		{
+			for(int j=row;j>=i;j--) 
+			{
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+		for(int i=row;i>=1;i--) {
+			for(int j=i;j<=row;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+	}
+
+}
