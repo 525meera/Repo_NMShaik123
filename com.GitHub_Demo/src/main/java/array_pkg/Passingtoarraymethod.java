10a6fd85631c90c0952fa03996264130d4e3c309
NMShaik <nmshaik525@gmail.com>
2023-07-17 16:54:37 +0530

    Create Passingtoarraymethod.java

---
+++ com.GitHub_Demo/src/main/java/array_pkg/Passingtoarraymethod.java
@@ -0,0 +1,22 @@
+package array_pkg;
+
+public class Passingtoarraymethod
+{
+	static void min(int arr[])
+	 {  
+		int min=arr[0];  
+		for(int i=1;i<arr.length;i++) 
+		{  
+		 if(min>arr[i])
+		 { 
+		  min=arr[i];
+		 }
+		}
+		System.out.println(min);  
+		}  
+		public static void main(String args[])
+		{  
+		int arr[]={33,3,4,5};// initializing  
+		min(arr);//passing array to method  
+		}
+}
