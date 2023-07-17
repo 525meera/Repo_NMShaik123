c9ba4b043b72b392475d8737f7956d44d9c9a5a1
NMShaik <nmshaik525@gmail.com>
2023-07-17 14:41:25 +0530

    Create Reverse_string.java

---
+++ com.GitHub_Demo/src/main/java/strings_pkg/Reverse_string.java
@@ -0,0 +1,16 @@
+package strings_pkg;
+
+public class Reverse_string 
+{
+	public static void main(String[] args) 
+	{
+		String str= "Shaik";
+		String rev="";
+		int length=str.length();
+		for(int i=length-1;i>=0;i--) 
+		{
+			rev=rev+str.charAt(i);
+		}
+		System.out.println(rev);
+	}
+}
