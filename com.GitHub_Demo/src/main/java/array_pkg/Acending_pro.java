97f11c3327ebf2d7178a2c4c437fc0c1e1432838
NMShaik <nmshaik525@gmail.com>
2023-07-18 10:59:31 +0530

    Create Acending_pro.java

---
+++ com.GitHub_Demo/src/main/java/array_pkg/Acending_pro.java
@@ -0,0 +1,37 @@
+package array_pkg;
+
+public class Acending_pro 
+{
+	public static void main(String[] args) 
+	{
+
+		int a[]	= {2,1,3,5,4};
+		int temp=0;
+		System.out.println("Before Ascending Oder");
+		for(int i=0;i<a.length;i++) {
+			System.out.print(a[i]);
+		}
+		System.out.println();
+		System.out.println("After Ascending Oder");
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=i+1;j<a.length;j++) 
+			{
+				if(a[i]>=a[j])
+				{
+					temp=a[i];
+					//System.out.print(temp);
+					a[i]=a[j];
+					//System.out.print(a[i]);
+					a[j]=temp;					
+					//System.out.print(a[j]);
+					//	System.out.println(temp);
+				}
+				//System.out.print(a[i]);
+			}
+			//System.out.print(temp);
+			System.out.print(a[i]);
+		}
+	}
+}
+
