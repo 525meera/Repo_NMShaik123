001ba68cd37b2f8cd6fc9b693912381658c9c358
NMShaik <nmshaik525@gmail.com>
2023-07-18 12:22:34 +0530

    Create Test_pro.java

---
+++ com.GitHub_Demo/src/main/java/static_pkg/Test_pro.java
@@ -0,0 +1,33 @@
+package static_pkg;
+
+public class Test_pro 
+{
+	static byte b=10;
+	static short s=20;
+	static long l=300000;
+	static String str="Meera";
+
+	int i=30;
+	float f=20;
+	double d=2000;
+
+	public static void m() 
+	{
+		System.out.println("Static Byte Values "+b);
+		System.out.println("Static Short values "+s);
+		System.out.println("Static long values "+l);
+		System.out.println("Static String name "+str);
+	}
+	public void m1()
+	{
+		System.out.println("Integer values  "+i);
+		System.out.println("Float values  "+f);
+		System.out.println("Double values  "+d);
+	}
+	public static void main(String[] args)
+	{
+		m();
+		Test_pro t=new Test_pro();
+		t.m1();
+	}
+}
