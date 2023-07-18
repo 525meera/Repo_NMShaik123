e9cdfaef1dcfe96ec7ae163e1f4007a157685733
NMShaik <nmshaik525@gmail.com>
2023-07-18 11:30:40 +0530

    Create Testarray1.java

---
+++ com.GitHub_Demo/src/main/java/array_pkg/Testarray1.java
@@ -0,0 +1,13 @@
+package array_pkg;
+
+public class Testarray1 
+{
+	public static void main(String[] args) 
+	{
+		int arr[]= {10,20,30,40};//initialization
+		for(int i=0;i<arr.length;i++) 
+		{
+			System.out.println(arr[i]);
+		}
+	}
+}
