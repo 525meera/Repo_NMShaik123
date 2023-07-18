ac146a01739acd671e7ca94c9c1ecc3fc3f9a083
NMShaik <nmshaik525@gmail.com>
2023-07-18 18:45:48 +0530

    Adding_Overrid

---
+++ com.GitHub_Demo/src/main/java/oops_pkg/Son.java
@@ -0,0 +1,11 @@
+package oops_pkg;
+
+public class Son extends Parent_pro
+{
+	void son() {
+		System.out.println("Son Call To Parent Class");
+	}
+	void kids() {
+		System.out.println("Kids call to my dady");
+	}
+}
