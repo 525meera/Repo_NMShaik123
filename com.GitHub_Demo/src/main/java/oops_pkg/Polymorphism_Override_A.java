303ebc1db49c72cb846b3a06c18dbeaf18ae2173
NMShaik <nmshaik525@gmail.com>
2023-07-19 17:41:12 +0530

    oops_override

---
+++ com.GitHub_Demo/src/main/java/oops_pkg/Polymorphism_Override_A.java
@@ -0,0 +1,15 @@
+package oops_pkg;
+
+public class Polymorphism_Override_A 
+{
+	String s;
+	int i=29;
+
+	void m1() {
+		System.out.println(s);
+	}
+	void m2() {
+		System.out.println(i);
+	}
+
+}
---
+++ com.GitHub_Demo/src/main/java/oops_pkg/Polymorphism_Override_B.java
@@ -0,0 +1,17 @@
+package oops_pkg;
+
+public class Polymorphism_Override_B extends Polymorphism_Override_A
+{
+
+	String s;
+	int i;
+
+	void m1() {
+		
+		System.out.println(s);
+	}
+	void m2() {
+		System.out.println(i);
+	}
+}
+
