2e346043768e58b91811b55cbfde9a94971e7126
NMShaik <nmshaik525@gmail.com>
2023-07-20 13:45:39 +0530

    Create Example.java

---
+++ com.GitHub_Demo/src/main/java/constracter/Example.java
@@ -0,0 +1,12 @@
+package constracter;
+
+public class Example 
+{
+	Example(){
+		System.out.println("Bike is Created");
+	}
+public static void main(String[] args) {
+	Example e=new Example();
+	
+}
+}
