3593e188127872942abaf7b0913d1daf0be277aa
NMShaik <nmshaik525@gmail.com>
2023-07-20 13:20:38 +0530

    Create Biggest_Numbers.java

---
+++ com.GitHub_Demo/src/main/java/control_statment/Biggest_Numbers.java
@@ -0,0 +1,28 @@
+package control_statment;
+
+public class Biggest_Numbers 
+{
+	public static void main(String[] args) {
+		
+		int n1=10;
+		int n2=30;
+		int n3=20;
+		
+		if((n1>n2) && (n1>n3)) 
+		{
+			System.out.println("This biggest value is n1");
+		}
+		else if((n2>n1) && (n2>n3)) 
+		{
+			System.out.println("This biggest value is n2");
+		}
+		else if((n3>n1) && (n3>n2)) 
+		{
+			System.out.println("This biggest value is n3");
+		}
+		else
+		{
+			System.out.println("All are Equles");
+		}
+		}
+}
