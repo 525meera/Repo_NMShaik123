7406383e3136c5fcea00f789cc27c386bf818b95
NMShaik <nmshaik525@gmail.com>
2023-07-20 15:16:35 +0530

    create_pro

---
+++ com.GitHub_Demo/src/main/java/super_Keyword/Example_Method.java
@@ -0,0 +1,11 @@
+package super_Keyword;
+
+public class Example_Method 
+{
+	int i=100;
+	String name="Meera";
+	void super_Method() 
+	{
+		System.out.println("Parent method ="+name+" "+i);
+	}
+}
---
+++ com.GitHub_Demo/src/main/java/super_Keyword/Method_to_excute.java
@@ -0,0 +1,10 @@
+package super_Keyword;
+
+public class Method_to_excute 
+{
+	public static void main(String[] args) {
+		Example_Method1 em=new Example_Method1();//child Class Object creating
+		em.super_Method();//Calling to parent Method Object
+		System.out.println();
+	}
+}
