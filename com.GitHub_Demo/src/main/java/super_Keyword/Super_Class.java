4f1dc4340c62aeda809f2b2febfeeda979c98e9d
NMShaik <nmshaik525@gmail.com>
2023-07-20 15:31:50 +0530

    addthepro

---
+++ com.GitHub_Demo/src/main/java/super_Keyword/Super_Class.java
@@ -0,0 +1,9 @@
+package super_Keyword;
+
+public class Super_Class 
+{
+	Super_Class()
+	{
+		System.out.println("Super_Class Constructor As Created");
+	}
+}
---
+++ com.GitHub_Demo/src/main/java/super_Keyword/Super_Class_main.java
@@ -0,0 +1,11 @@
+package super_Keyword;
+
+public class Super_Class_main 
+{
+	public static void main(String[] args) {
+
+
+		Super_Class_1 sc=new Super_Class_1();//Calling to parent class Constructor
+		
+	}
+}
