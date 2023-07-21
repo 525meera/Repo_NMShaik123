72a5708b606ac49f5517f5d4c77d20f98e4f53cf
NMShaik <nmshaik525@gmail.com>
2023-07-21 12:21:17 +0530

    interface_pro adding

---
+++ com.GitHub_Demo/src/main/java/interface_pkg/Drawable.java
@@ -0,0 +1,6 @@
+package interface_pkg;
+
+public interface Drawable 
+{
+	void draw();
+}
---
+++ com.GitHub_Demo/src/main/java/interface_pkg/Rectangle.java
@@ -0,0 +1,10 @@
+package interface_pkg;
+
+public class Rectangle implements Drawable{
+	public void draw()
+	{
+		System.out.println("Rectangle");
+	}
+
+
+}
