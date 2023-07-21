eb880a39526b743e04429a0e1b8b56725953b0bb
NMShaik <nmshaik525@gmail.com>
2023-07-21 12:12:26 +0530

    interface_pro

---
+++ com.GitHub_Demo/src/main/java/interface_pkg/A.java
@@ -0,0 +1,8 @@
+package interface_pkg;
+
+
+interface  A 
+{
+	void m1();
+	void m2();
+}
---
+++ com.GitHub_Demo/src/main/java/interface_pkg/Excute_main.java
@@ -0,0 +1,11 @@
+package interface_pkg;
+
+public class Excute_main 
+{
+	public static void main(String[] args) 
+	{
+     B b=new B();
+     b.m1();
+     b.m2();
+	}
+}
