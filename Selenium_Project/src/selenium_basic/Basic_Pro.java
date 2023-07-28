69769a3752bf335c778c1773bc49cb027d23c237
NMShaik <nmshaik525@gmail.com>
2023-07-28 22:46:08 +0530

    commit to main

---
+++ Selenium_Project/src/selenium_basic/Basic_Pro.java
@@ -0,0 +1,27 @@
+package selenium_basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Basic_Pro 
+{
+	public static void main(String[] args) throws InterruptedException 
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://practicetestautomation.com/practice-test-login/");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		String get_Title=driver.getTitle();
+		System.out.println(get_Title);
+		Thread.sleep(2000);
+		String get_CurrentUrl=driver.getCurrentUrl();
+		System.out.println(get_CurrentUrl);
+		Thread.sleep(4000);
+		String get_PageSource=driver.getPageSource();
+		System.out.println(get_PageSource);
+		Thread.sleep(2000);
+		
+		driver.close();
+
+	}
+}
